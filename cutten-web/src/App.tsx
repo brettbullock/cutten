953aa84f7722db921a234044114ead463fba5f26
breadbullock <brett.r.bullock@gmail.com>
2019-07-12 15:28:38 -0700

    Strapped Apollo Client onto cutten-web

--- cutten-web/src/App.tsx
+++ cutten-web/src/App.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { gql } from "apollo-boost";
+import { Query } from "react-apollo";
 
 import {
   ThemeProvider,
@@ -6,14 +8,29 @@ import {
   Notice
 } from 'kingsbury/lib';
 
+const TEST_QUERY = gql`
+  query {
+    hello
+  }
+`
+
 export const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Notice
-        type="info"
-        title="Hello from Kingbury"
-        description="got it to workkk"
-      />
-    </ThemeProvider>
+    <Query query={TEST_QUERY}>
+      {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error</p>;
+
+      return (
+        <ThemeProvider theme={theme}>
+          <Notice
+            type="info"
+            title="Hello from Kingbury"
+            description={data.hello}
+          />
+        </ThemeProvider>
+      )
+    }}
+    </Query>
   );
 }
--- cutten-web/src/index.tsx
+++ cutten-web/src/index.tsx
@@ -1,7 +1,19 @@
 import * as React from "react";
 import { render } from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
 import "./index.css";
 
 import { App } from "./App";
 
-render(<App />, document.getElementById("root"));
+const client = new ApolloClient({
+    uri: "http://localhost:8000/graphql"
+});
+
+render (
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, 
+    document.getElementById("root")
+);
