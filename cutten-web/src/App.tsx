83f959f2da202f8d8b11e6b0e962138bfd5914e5
breadbullock <52832733+breadbullock@users.noreply.github.com>
2019-07-13 15:32:55 -0700

    Strapped Apollo Client onto cutten-web (#2)
    * Strapped Apollo Client onto cutten-web
    
    * updating the commit to fix some things
    
    * modifying the query to work with ts

--- cutten-web/src/App.tsx
+++ cutten-web/src/App.tsx
@@ -1,4 +1,12 @@
-import * as React from "react";
+import * as React from 'react';
+
+import {
+  gql
+} from 'apollo-boost';
+
+import {
+  Query
+} from 'react-apollo';
 
 import {
   ThemeProvider,
@@ -6,14 +14,49 @@ import {
   Notice
 } from 'kingsbury/lib';
 
-export const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <Notice
-        type="info"
-        title="Hello from Kingbury"
-        description="got it to workkk"
-      />
-    </ThemeProvider>
-  );
-}
+export interface Data {
+  hello: string;
+};
+
+const TEST_QUERY = gql`
+  query {
+    hello
+  }
+`;
+
+
+export const App = () => (
+  <Query<Data, any> query={TEST_QUERY}>
+    {({ loading, error, data }) => {
+
+      if (loading) {
+        return <p>Loading...</p>
+      };
+
+      if (error) {
+        return <p>Error</p>;
+      };
+
+      // need to handle this case just because ts will complain because data
+      // could be undefined
+      if (!data) {
+        return <p>No data</p>;
+      }
+
+      const {
+        hello
+      } = data;
+
+      return (
+        <ThemeProvider theme={theme}>
+          <Notice
+            type="info"
+            title="Hello from Kingbury"
+            description={hello}
+          />
+        </ThemeProvider>
+      );
+    }}
+  </Query>
+);
+
--- cutten-web/src/index.tsx
+++ cutten-web/src/index.tsx
@@ -1,7 +1,28 @@
-import * as React from "react";
-import { render } from "react-dom";
-import "./index.css";
+import * as React from 'react';
 
-import { App } from "./App";
+import {
+  default as ApolloClient
+}  from 'apollo-boost';
 
-render(<App />, document.getElementById("root"));
+import {
+  render
+} from 'react-dom';
+
+import {
+  ApolloProvider
+} from 'react-apollo';
+
+import {
+  App
+} from './App';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql'
+});
+
+render (
+  <ApolloProvider client={client}>
+      <App />
+  </ApolloProvider>, 
+  document.getElementById('root')
+);
